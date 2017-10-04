bb5292679c996c7bd2e3c633c76a8f7f55cce304
Dhia ben maati <32402326+dhiabm@users.noreply.github.com>
2017-10-04 21:32:59 +0200

    Update Tp2Ex4.c

--- Tp2Ex4/src/Tp2Ex4.c
+++ Tp2Ex4/src/Tp2Ex4.c
@@ -1,25 +1,15 @@
-/*
- ============================================================================
- Name        : Tp2Ex4.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	int ann;
-	printf("taper une année");
-	scanf("%d",&ann);
-	if(ann % 400 == 0)
-		printf("%d est bissextile",ann);
-	else
-		if((ann % 4 ==0 )&(ann % 100 !=0))
-			printf("%d est bissextile",ann);
-		else printf("%d n'est pas bissextile",ann);
+int main()
+{
+int x;
+printf("taper l'anne pls");
+scanf("%d",&x);
+if (((x%4==0)&&(x%100!=0))||(x%400==0)) {printf("l'anne est bissextile");}
+else {printf("l'anne n'est pas bissextile");}
+
+    return 0;
+}
 
 }
